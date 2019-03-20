939c45c406c03a13f343b8bcec6d877a3b90e7cf
tangkaiyang <945541696@qq.com>
2019-03-20 09:42:20 +0800

    错误,调试和测试

---
+++ chapter08/fact_doc_test.py
@@ -0,0 +1,27 @@
+# -*- coding:UTF-8 -*-
+
+
+# 对函数fact(n)编写doctest并执行
+def fact(n):
+    """
+    Calculate 1*2*...*n
+
+    >>> fact(1)
+    1
+    >>> fact(10)
+    3628800
+    >>> fact(-1)
+    Traceback (most recent call last):
+        ...
+    ValueError: 'n' must be positive
+    """
+    if n < 1:
+        raise ValueError("'n' must be positive")
+    if n == 1:
+        return 1
+    return n * fact(n - 1)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
