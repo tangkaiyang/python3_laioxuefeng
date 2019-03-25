611637627640463abd689fefed3f424de6766d0f
tangkaiyang <945541696@qq.com>
2019-03-25 16:11:44 +0800

    UDP

---
+++ chapter15/udp_server.py
@@ -0,0 +1,14 @@
+#  -*- coding:UTF-8 -*-
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 绑定端口
+s.bind(('127.0.0.1', 9999))
+# 直接接受来自任何客户端的数据:
+print('Bind UDP on 9999...')
+while True:
+    # 接受数据
+    data, addr = s.recvfrom(1024)
+    print('Received from %s:%s.' % addr)
+    s.sendto(b'Hello, %s!' % data, addr)
