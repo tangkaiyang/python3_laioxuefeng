8b7d77f50d735d7b7bbd4872323db04ef800ba39
tangkaiyang <945541696@qq.com>
2019-03-26 16:14:39 +0800

    连接数据库,ORM

---
+++ chapter17/sql_test.py
@@ -0,0 +1,41 @@
+#  -*- coding:UTF-8 -*-
+
+"""
+练习:
+请编写函数,在Sqlite中根据分数段查找指定的名字:
+"""
+import os, sqlite3
+
+db_file = os.path.join(os.path.dirname(__file__), 'test.db')
+if os.path.isfile(db_file):
+    os.remove(db_file)
+
+# 初始数据:
+conn = sqlite3.connect(db_file)
+cursor = conn.cursor()
+cursor.execute('create table user(id varchar(20) primary key, name varchar(20), score int)')
+cursor.execute(r"insert into user values ('A-001', 'Adam', 95)")
+cursor.execute(r"insert into user values ('A-002', 'Bart', 62)")
+cursor.execute(r"insert into user values ('A-003', 'Lisa', 78)")
+cursor.close()
+conn.commit()
+conn.close()
+
+
+def get_score_in(low, high):
+    '返回指定分数区间的名字,按分数从低到高排序'
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    try:
+        cursor.execute('select name from user where score >= ? and score <= ?', (low, high))
+        values = cursor.fetchall()
+    finally:
+        cursor.close()
+        conn.commit()
+        conn.close()
+    return values
+
+
+print(get_score_in(80, 95))
+print(get_score_in(60, 80))
+print(get_score_in(60, 100))
